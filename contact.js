a9f7764d82d34999ecedb2ee12ea157aa5f8254b
yuslianto <yuslianto56@gmail.com>
2019-07-11 12:13:09 +0700

    adding contact and open form contact wit react native contacts

--- contact.js
+++ contact.js
@@ -7,10 +7,15 @@ import {
     PermissionsAndroid,
     Platform,
     requestMultiple,
+    ScrollView,
 } from "react-native";
 import Contacts from 'react-native-contacts';
 
 class ContactComponents extends Component {
+
+    state = {
+        myContact : []
+    }
 
     async requestCOnatctPermission(){
         if (Platform.OS === "ios") {
@@ -32,7 +37,61 @@ class ContactComponents extends Component {
         }
     }
     
-    getContacts= () => {
+    addContacts = () => {
+        this.requestCOnatctPermission()
+        .then((didGetPermission)=>{
+            if (didGetPermission) {
+
+                const newContact = {
+                    emailAddresses: [{
+                        label: "work",
+                        email: "carl-jung@example.com",
+                    }],
+                    familyName: "Jung",
+                    givenName: "Carl",
+                    phoneNumbers: [{
+                        label: "mobile",
+                        number: "(555) 555-5555",
+                    }],
+                    
+                }
+                Contacts.addContact(newContact, (err) => {
+                    if (err) throw err;
+                    // save successful
+                    this.getContacts();
+                })
+
+
+            } else {
+                alert("No Permission")
+            }
+        })
+    }
+
+    openContactsForm = () => {
+
+        const newContact = {
+            emailAddresses: [{
+                label: "work",
+                email: "carl-jung@example.com",
+            }],
+            familyName: "Jung",
+            givenName: "Carl",
+            phoneNumbers: [{
+                label: "mobile",
+                number: "(555) 555-5555",
+            }],
+            
+        }
+
+        Contacts.openContactForm(newContact, (err)=>{
+            if (err) {
+                console.warn(err);
+            }
+        })
+    }
+
+    getContacts = () => {
         this.requestCOnatctPermission()
         .then((didGetPermission)=>{
             if (didGetPermission) {
@@ -41,7 +100,10 @@ class ContactComponents extends Component {
                       throw err;
                     }
                     // contacts returned
-                    console.warn(contacts);
+                    //console.warn(contacts);
+                    this.setState({
+                        myContact:contacts
+                    })
                   })
             } else {
                 alert("No Permission")
@@ -53,10 +115,25 @@ class ContactComponents extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <ScrollView>
+                    {
+                        this.state.myContact.map((item, i)=>(
+                            <Text key={i}>{item.givenName} {item.familyName}</Text>
+                        ))
+                    }
+                </ScrollView>
                 <Text>contact Components</Text>
                 <Button
                     title="Load Contacts"
                     onPress={this.getContacts}
+                />
+                <Button
+                    title="Add Contacts"
+                    onPress={this.addContacts}
+                />
+                <Button
+                    title="Open Form Contacts"
+                    onPress={this.openContactsForm}
                 />
             </View>
         );
@@ -67,5 +144,8 @@ export default ContactComponents;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
+        //alignItems: 'center'
+        alignContent: 'center'
     }
 });
