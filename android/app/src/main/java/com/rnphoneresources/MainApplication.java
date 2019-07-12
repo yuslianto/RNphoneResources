4ec1208b9f882026c0364979e4e71fe537274eca
yuslianto <yuslianto56@gmail.com>
2019-07-12 11:26:33 +0700

    install react native vector icons

--- android/app/src/main/java/com/rnphoneresources/MainApplication.java
+++ android/app/src/main/java/com/rnphoneresources/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new ImagePickerPackage(),
-          new ReactNativeContacts());
+          new ReactNativeContacts(),
+          new VectorIconsPackage()
+      );
     }
 
     @Override
--- contact.js
+++ contact.js
@@ -8,8 +8,10 @@ import {
     Platform,
     requestMultiple,
     ScrollView,
+    TouchableOpacity
 } from "react-native";
 import Contacts from 'react-native-contacts';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class ContactComponents extends Component {
 
@@ -122,19 +124,48 @@ class ContactComponents extends Component {
                         ))
                     }
                 </ScrollView>
-                <Text>contact Components</Text>
-                <Button
-                    title="Load Contacts"
+                <View style={styles.navTitleStyle}>
+                    <Text style={styles.textTitleStyle}>Contact Components</Text>
+                </View>
+
+                <TouchableOpacity
                     onPress={this.getContacts}
-                />
-                <Button
-                    title="Add Contacts"
+                    style={styles.buttonStyle}
+                >
+                    <Icon name="ios-refresh-circle" 
+                        color="white"
+                        size={20}
+                    />
+                    <Text style={styles.textButtonStyle}>
+                        Load Contacts
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
                     onPress={this.addContacts}
-                />
-                <Button
-                    title="Open Form Contacts"
+                    style={styles.buttonStyle}
+                >
+                    <Icon name="ios-add-circle"
+                        color="white"
+                        size={20}
+                    />
+                    <Text style={styles.textButtonStyle}>
+                        Add Contacts
+                    </Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity
                     onPress={this.openContactsForm}
-                />
+                    style={styles.buttonStyle}
+                >
+                    <Icon name="md-contacts" 
+                        color="white"
+                        size={20}
+                    />
+                    <Text style={styles.textButtonStyle}>
+                        Open Form Contacts
+                    </Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -147,5 +178,31 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         //alignItems: 'center'
         alignContent: 'center'
-    }
+    },
+    navTitleStyle:{
+        alignItems: 'center',
+        height: 50,
+        backgroundColor: 'grey',
+        justifyContent: 'center'
+    },
+    textTitleStyle: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    buttonStyle: {
+        alignItems: 'center',
+        height: 50,
+        marginTop: 5,
+        backgroundColor:"#3b5998",
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    textButtonStyle: { 
+        fontFamily: 'Arial', 
+        fontSize: 15, 
+        color: 'white',
+        fontWeight: 'bold',
+        paddingHorizontal: 10
+    },
 });
