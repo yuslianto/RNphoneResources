6ad8baa535e5cf14a4b7cba9dc62f7fa28bfc9a3
yuslianto <yuslianto56@gmail.com>
2019-07-16 19:31:43 +0700

    learning simple animated spring

--- src/AnimationLearn/AnimOne.js
+++ src/AnimationLearn/AnimOne.js
@@ -2,14 +2,40 @@ import React, { Component } from "react";
 import { 
     View,
     Text,
+    Animated,
     StyleSheet
 } from "react-native";
 
 class AnimOne extends Component {
+
+    constructor() {
+        super();
+        this.redSquare = new Animated.ValueXY(0, 0)
+    }
+
+    componentDidMount() {
+        Animated.spring(this.redSquare,{
+            toValue: {
+                x: 100,
+                y: 300
+            }
+        }).start();
+    }
+
     render() {
         return (
-            <View style={styles.squre}>
-            </View>
+            <Animated.View
+                style={this.redSquare.getLayout()}
+                /**
+                style={{
+                    left: this.redSquare.x,
+                    top: this.redSquare.y
+                }}
+                */
+            >
+                <View style={styles.squre}>
+                </View> 
+            </Animated.View>
         );
     }
 }
