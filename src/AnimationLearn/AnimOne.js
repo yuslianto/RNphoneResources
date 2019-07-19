413c4d64dcd67caee23d3a8b08024ec254371f01
yuslianto <yuslianto56@gmail.com>
2019-07-19 17:32:13 +0700

    edit animation interpolate

--- src/AnimationLearn/AnimOne.js
+++ src/AnimationLearn/AnimOne.js
@@ -25,8 +25,8 @@ class AnimOne extends Component {
     runYellowSquare = () => {
         Animated.timing(this.state.yellowSquare,{
             toValue: 0,
-            duration: 2000,
-            delay: 1000,
+            duration: 5000,
+            //delay: 1000,
             //easing: Easing.elastic(4)
         }).start();
     }
@@ -34,7 +34,7 @@ class AnimOne extends Component {
     runGreenSquare = () => {
         Animated.timing(this.state.greenSquare,{
             toValue: 1,
-            duration: 2000,
+            duration: 5000,
             delay: 1000,
             //easing: Easing.elastic(4)
         }).start();
@@ -46,7 +46,7 @@ class AnimOne extends Component {
                 x: 100,
                 y: 300
             },
-            duration: 2000,
+            duration: 5000,
             delay: 1000,
             easing: Easing.elastic(4)
         }).start();
@@ -85,21 +85,27 @@ class AnimOne extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View
-                    style={styles.navSquare}
-                >
+
+                <View style={styles.navSquare}>
                     <Animated.View
                         style={{
                             opacity: this.state.yellowSquare,
-                            left: this.state.yellowSquare.interpolate({
-                                inputRange:[0,1],
-                                outputRange:[300,0]
-                            })
+                            //left: this.state.yellowSquare.interpolate({
+                            //    inputRange:[0,1],
+                            //    outputRange:[300,0]
+                            //}),
+                            transform:[{
+                                rotateX: this.state.yellowSquare.interpolate({
+                                    inputRange: [0,0.5,1],
+                                    outputRange:['0deg','180deg','0deg']
+                                })
+                            }]
                         }}
                     >
-                        <View
-                            style={styles.yellowSq}
-                        >
+                        <View style={styles.yellowSq}>
+                            <Text style={styles.textStyle}>
+                                Yellow
+                            </Text>
                         </View>
                     </Animated.View>
 
@@ -108,9 +114,10 @@ class AnimOne extends Component {
                             opacity: this.state.greenSquare
                         }}
                     >
-                        <View
-                            style={styles.greenSq}
-                        >
+                        <View style={styles.greenSq}>
+                            <Text style={styles.textStyle}>
+                                Green
+                            </Text>
                         </View>
                     </Animated.View>
 
@@ -123,7 +130,10 @@ class AnimOne extends Component {
                         }}
                         */
                     >
-                        <View style={styles.squre}>
+                        <View style={styles.redSq}>
+                            <Text style={styles.textStyle}>
+                                Red
+                            </Text>
                         </View> 
                     </Animated.View>
 
@@ -131,12 +141,37 @@ class AnimOne extends Component {
                         style={this.state.blueSquare.getLayout()}
                     >
                         <View style={styles.blueSq}>
+                            <Text style={styles.textStyle}>
+                                Blue
+                            </Text>
                         </View>
                     </Animated.View>
                 </View>
-                <View
-                    sttyle={styles.navButtonStyle}
-                >
+
+                <View style={styles.navTextStyle}>
+                    <Animated.Text
+                        style={{
+                            fontSize: this.state.yellowSquare.interpolate({
+                                inputRange:[0,0.5,1],
+                                outputRange:[50,0,100]
+                            }),
+                            color: this.state.yellowSquare.interpolate({
+                                inputRange:[0,1],
+                                outputRange:['blue','red']
+                            }),
+                            transform:[{
+                                rotateX: this.state.yellowSquare.interpolate({
+                                    inputRange: [0,0.5,1],
+                                    outputRange:['0deg','180deg','0deg']
+                                })
+                            }]
+                        }}
+                    >
+                        <Text>Yellow</Text>
+                    </Animated.Text>
+                </View>
+                
+                <View sttyle={styles.navButtonStyle}>
                     <Button
                         title="run animation red square"
                         onPress={this.runRedSquare}
@@ -158,6 +193,7 @@ class AnimOne extends Component {
                         color='yellow'
                     />
                 </View>
+
             </View>
         );
     }
@@ -172,15 +208,19 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row'
     },
-    squre: {
+    redSq: {
         width: 100,
         height: 100,
-        backgroundColor: 'red'
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     blueSq:{
         height: 100,
         width: 100,
-        backgroundColor: 'blue'
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     navButtonStyle:{
         margin: 10,
@@ -190,11 +230,25 @@ const styles = StyleSheet.create({
     greenSq:{
         height: 100,
         width: 100,
-        backgroundColor: 'green'
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     yellowSq:{
         width:100,
         height:100,
-        backgroundColor: 'yellow'
+        backgroundColor: 'yellow',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    navTextStyle:{
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textStyle:{
+        color: 'black',
+        fontFamily: 'arial',
+        fontSize: 20,
+        fontWeight: 'bold'
     }
 });
